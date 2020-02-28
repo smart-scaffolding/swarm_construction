2a35ae563e9ac383bbe2747fb01ad4677697ac34
CWagner23 <calebwagner23@gmail.com>
2020-02-28 13:06:29 -0500

    feat(all): Additional ZMQ testing
    Various tests in ZMQ including Paranoid Pirate protocol
    
    Ref:

---
+++ zmq_tests/req_rep_client.py
@@ -0,0 +1,36 @@
+#
+#   Request-reply client in Python
+#   Connects REQ socket to tcp://localhost:5559
+#   Sends "Hello" to server, expects "World" back
+#
+import zmq
+import time
+
+# context = zmq.Context()
+# socket = context.socket(zmq.REP)
+# socket.connect("tcp://127.0.0.1:5560")
+
+
+ports = ["tcp://127.0.0.1:5561"]
+
+context = zmq.Context()
+# print("Connecting to machine...")
+
+sockets = []
+for index in range(len(ports)):
+    context = zmq.Context()
+    print("Connecting to machine...")
+    socket = context.socket(zmq.REP)
+    # socket.setsockopt(zmq.LINGER, 0)
+    # socket.bind("%s" % ports[index])
+    socket.connect(ports[index])
+    print("Successfully connected to machine %s" % ports[index])
+    sockets.append(socket)
+
+
+while True:
+    for index in range(len(ports)):
+        message = sockets[index].recv_string()
+        print("Sending position to structure: ", message)
+        sockets[index].send_string(f"Position: {ports[index]}")
+        # time.sleep(3)
---
+++ zmq_tests/req_rep_server.py
@@ -0,0 +1,20 @@
+#
+#   Request-reply service in Python
+#   Connects REP socket to tcp://localhost:5560
+#   Expects "Hello" from client, replies with "World"
+#
+
+import zmq
+import time
+#  Prepare our context and sockets
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+socket.bind("tcp://127.0.0.1:5561")
+
+#  Do 10 requests, waiting each time for a response
+for request in range(1,11):
+    print(f"Requesting position: [{request}/10]")
+    socket.send_string(f"Please tell me your position: {request}")
+    message = socket.recv_string()
+    print("Received position %s [%s]" % (request, message))
+    time.sleep(7)
